fa2aa4b3cb65a23484326946982e9a12a44f875a
paolomococci <paolomococci@gmail.com>
2021-10-28 17:23:03 +0200

    I have added some examples of Angular Material components to the recipe-zero project

--- recipe-zero/src/app/app.module.ts
+++ recipe-zero/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -13,6 +19,12 @@ import { MatSliderModule } from '@angular/material/slider';
   ],
   imports: [
     MatSliderModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatTabsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
