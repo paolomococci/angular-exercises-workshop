8775269f6e5d8119d614f328b3ebb57a73c1b0d7
paolomococci <paolomococci@gmail.com>
2022-04-30 00:13:03 +0200

    I added file notification-button.component.ts to exercise exerc01

---
+++ components_communication/exerc01/src/app/notifications-button/notifications-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-button',
+  templateUrl: './notifications-button.component.html',
+  styleUrls: ['./notifications-button.component.sass']
+})
+export class NotificationsButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
