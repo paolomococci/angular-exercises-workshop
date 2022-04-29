88b5781c6953a36e8ca505461cfd51a389bdf680
paolomococci <paolomococci@gmail.com>
2022-04-29 23:37:54 +0200

    I edited file app.module.ts to exercise exerc01

--- components_communication/exerc01/src/app/app.module.ts
+++ components_communication/exerc01/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationsManagerComponent } from './notifications-manager/notifications-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsManagerComponent
   ],
   imports: [
     BrowserModule,
