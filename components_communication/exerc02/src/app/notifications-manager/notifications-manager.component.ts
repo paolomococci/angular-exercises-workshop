d88f8623f3febebea2e9c7d87dd12896efe9c113
paolomococci <paolomococci@gmail.com>
2022-04-30 10:08:43 +0200

    I modified file notifications-manager.componet.ts belonging to exercise exerc02

--- components_communication/exerc02/src/app/notifications-manager/notifications-manager.component.ts
+++ components_communication/exerc02/src/app/notifications-manager/notifications-manager.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'app-notifications-manager',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsManagerComponent implements OnInit {
 
+  @Input() count = 0
+
+  @Output() countChanged = new EventEmitter<number>()
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  addNotification() {
+    this.count++
+    this.countChanged.emit(this.count)
+  }
+
+  removeNotification() {
+    if (this.count == 0) {
+      return
+    }
+    this.count--
+    this.countChanged.emit(this.count)
+  }
+
+  resetCount() {
+    this.count = 0
+    this.countChanged.emit(this.count)
   }
 
 }
