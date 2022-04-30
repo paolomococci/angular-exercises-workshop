99c75d1e4927b562275927119f6e64fc740509a0
paolomococci <paolomococci@gmail.com>
2022-04-30 09:52:04 +0200

    I added file notifications-manager.component.ts to exercise exerc02

---
+++ components_communication/exerc02/src/app/notifications-manager/notifications-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-manager',
+  templateUrl: './notifications-manager.component.html',
+  styleUrls: ['./notifications-manager.component.sass']
+})
+export class NotificationsManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
