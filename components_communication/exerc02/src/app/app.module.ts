716557999754c748af48f8c80a73e8169a669b93
paolomococci <paolomococci@gmail.com>
2022-05-01 08:46:33 +0200

    I modified file app.module.ts belonging to exercise exerc02

--- components_communication/exerc02/src/app/app.module.ts
+++ components_communication/exerc02/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NotificationsManagerComponent } from './notifications-manager/notifications-manager.component';
-import { NotificationsButtonComponent } from './notifications-button/notifications-button.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NotificationsManagerComponent } from './notifications-manager/notifications-manager.component'
+import { NotificationsButtonComponent } from './notifications-button/notifications-button.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
