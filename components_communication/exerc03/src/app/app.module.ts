632c79141d544d5c8a8fa74ac3b4c3268fd726ce
paolomococci <paolomococci@gmail.com>
2022-05-01 17:41:04 +0200

    I have edited the app.module.ts file belonging to exercise exerc03

--- components_communication/exerc03/src/app/app.module.ts
+++ components_communication/exerc03/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VcLogsComponent } from './components/vc-logs/vc-logs.component';
+import { VersionControlComponent } from './components/version-control/version-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VcLogsComponent,
+    VersionControlComponent
   ],
   imports: [
     BrowserModule,
