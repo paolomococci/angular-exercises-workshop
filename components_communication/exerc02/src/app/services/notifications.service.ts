b9f691cf5cd06ffc19a9ef1363c924712d786fb6
paolomococci <paolomococci@gmail.com>
2022-05-01 08:40:32 +0200

    I modified file notifications.service.ts belonging to exercise exerc02

--- components_communication/exerc02/src/app/services/notifications.service.ts
+++ components_communication/exerc02/src/app/services/notifications.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+
+  private count: BehaviorSubject<number> = new BehaviorSubject<number>(0)
 
   constructor() { }
 }
