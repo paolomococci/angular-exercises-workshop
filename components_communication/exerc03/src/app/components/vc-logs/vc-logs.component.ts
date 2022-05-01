88b864cfcf226df4eed57c293f665e2c55aabd0b
paolomococci <paolomococci@gmail.com>
2022-05-01 17:42:55 +0200

    I added file vc-logs.component.ts to exercise exerc03

---
+++ components_communication/exerc03/src/app/components/vc-logs/vc-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vc-logs',
+  templateUrl: './vc-logs.component.html',
+  styleUrls: ['./vc-logs.component.sass']
+})
+export class VcLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
