98b7df6cb247a72a10a15ff64f10bbf4df06ab19
paolomococci <paolomococci@gmail.com>
2022-05-01 18:50:58 +0200

    I added file version-control.component.ts to exercise exerc03

--- components_communication/exerc03/src/app/components/version-control/version-control.component.ts
+++ components_communication/exerc03/src/app/components/version-control/version-control.component.ts
@@ -29,6 +29,11 @@ export class VersionControlComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.versionForm.get(
+      'version'
+    )?.setValue(
+      this.versionName
+    )
   }
 
 }
