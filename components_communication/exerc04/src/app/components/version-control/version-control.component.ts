dc16437d09468cff276d1c410991f8213064d7a8
paolomococci <paolomococci@gmail.com>
2022-05-03 17:25:44 +0200

    I have edited the version-control.component.ts file belonging to exercise exerc04

--- components_communication/exerc04/src/app/components/version-control/version-control.component.ts
+++ components_communication/exerc04/src/app/components/version-control/version-control.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms'
 
 @Component({
   selector: 'app-version-control',
@@ -7,9 +15,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VersionControlComponent implements OnInit {
 
+  versionForm = new FormGroup(
+    {
+      version: new FormControl(
+        '',
+        [Validators.pattern('([0-9]+)\.([0-9]+)\.([0-9]+)')]
+      )
+    }
+  )
+
+  versionName = '0.0.0'
+
   constructor() { }
 
   ngOnInit(): void {
+    this.versionForm.get(
+      'version'
+    )?.setValue(
+      this.versionName
+    )
+  }
+
+  formSubmit() {
+    if (!this.versionForm.valid) {
+      alert('version has an invalid format')
+      return
+    }
+    this.versionName = this.versionForm.get(
+      'version'
+    )?.value
   }
 
 }
