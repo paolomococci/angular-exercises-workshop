f1312bfd1cee97d52f216cc3c449cfc6f3814990
paolomococci <paolomococci@gmail.com>
2022-05-03 17:15:58 +0200

    I have edited the vc-logs.component.ts file belonging to exercise exerc04

--- components_communication/exerc04/src/app/components/vc-logs/vc-logs.component.ts
+++ components_communication/exerc04/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vc-logs.component.sass']
 })
 export class VcLogsComponent implements OnInit {
+
+  @Input()
+  vName!: string
+
+  logs: string[] = []
 
   constructor() { }
 
