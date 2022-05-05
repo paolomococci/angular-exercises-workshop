855ef95653dbd7d02062f8499fbec438a23421c1
paolomococci <paolomococci@gmail.com>
2022-05-05 15:50:40 +0200

    I have edited the gallery.component.ts file belonging to exercise exerc06

--- components_communication/exerc06/src/app/components/gallery/gallery.component.ts
+++ components_communication/exerc06/src/app/components/gallery/gallery.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  images: string[] = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.images = new Array(5).fill(0).map(this.retrieveImage)
+  }
+
+  retrieveImage(): string {
+    throw new Error('Method not implemented.')
   }
 
 }
