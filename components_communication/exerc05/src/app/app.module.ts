d409977a901ea8a13a774c6f444fd6a63a97df50
paolomococci <paolomococci@gmail.com>
2022-05-05 15:29:58 +0200

    I have edited the app.module.ts file belonging to exercise exerc05

--- components_communication/exerc05/src/app/app.module.ts
+++ components_communication/exerc05/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { GalleryComponent } from './gallery/gallery.component'
+import { GalleryComponent } from './components/gallery/gallery.component'
 
 @NgModule({
   declarations: [
