660b4966038b6180aa0077e30cc29c33995dbad0
paolomococci <paolomococci@gmail.com>
2022-05-06 18:44:43 +0200

    I added file chirping-card.component.ts to exercise exerc07

---
+++ components_communication/exerc07/src/app/components/chirping-card/chirping-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chirping-card',
+  templateUrl: './chirping-card.component.html',
+  styleUrls: ['./chirping-card.component.sass']
+})
+export class ChirpingCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
