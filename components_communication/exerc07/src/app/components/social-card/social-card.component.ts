38caf6bab474a4d2e9eb6df23320e468cec95fb6
paolomococci <paolomococci@gmail.com>
2022-05-07 16:25:11 +0200

    I have edited the social-card.component.ts file belonging to exercise exerc07

--- components_communication/exerc07/src/app/components/social-card/social-card.component.ts
+++ components_communication/exerc07/src/app/components/social-card/social-card.component.ts
@@ -33,7 +33,7 @@ export class SocialCardComponent implements OnInit {
 
   cardType!: SocialCardType
 
-  constructor() { }
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
   }
