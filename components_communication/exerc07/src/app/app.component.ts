6d5a71bf11d38febd2866213e0b87ab6c17b22ed
paolomococci <paolomococci@gmail.com>
2022-05-08 10:52:19 +0200

    I have edited the app.component.ts file belonging to exercise exerc07

--- components_communication/exerc07/src/app/app.component.ts
+++ components_communication/exerc07/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { SocialCardType } from './enums/social-card-type'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'exerc07'
+
+  selectedCardType!: SocialCardType
 }
