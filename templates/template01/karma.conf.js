45f7c7b53ec8d0d986dcf4123f3fb3de9bc495ab
paolomococci <37364757+paolomococci@users.noreply.github.com>
2022-05-10 08:03:08 +0200

    Update karma.conf.js

--- templates/template01/karma.conf.js
+++ templates/template01/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/template01'),
+      dir: require('path').join(__dirname, './coverage/welcome'),
       subdir: '.',
       reporters: [
         { type: 'html' },
