ce027faca7d5ee4007b416d9bcc7edde486cbc34
paolomococci <paolomococci@gmail.com>
2022-05-23 06:07:39 +0200

    I have edited the highlight.directive.ts file belonging to exercise exerc08

--- directives/exerc08/src/app/directives/highlight.directive.ts
+++ directives/exerc08/src/app/directives/highlight.directive.ts
@@ -1,16 +1,22 @@
 import {
   Directive,
-  Input
+  Input,
+  OnChanges,
+  SimpleChanges
 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
 
   @Input() highlightText = ''
   @Input() highlightColor = '#ff6'
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
