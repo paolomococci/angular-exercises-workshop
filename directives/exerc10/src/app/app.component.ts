b19e42b5ca30a6113b69eef7abc7026e81923688
paolomococci <paolomococci@gmail.com>
2022-05-23 18:07:44 +0200

    I have edited the app.component.ts file belonging to exercise exerc10

--- directives/exerc10/src/app/app.component.ts
+++ directives/exerc10/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'exerc10'
+  readTime!: string
+
+  onReadTimeComputed(readTimeString: string) {
+    this.readTime = readTimeString
+  }
 }
