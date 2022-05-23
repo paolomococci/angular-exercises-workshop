68d2941d62758337c0f5ebf865a8923c38525fdb
paolomococci <paolomococci@gmail.com>
2022-05-23 07:57:15 +0200

    I have edited the highlight.directive.ts file belonging to exercise exerc09

--- directives/exerc09/src/app/directives/highlight.directive.ts
+++ directives/exerc09/src/app/directives/highlight.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core'
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core'
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
 
-  constructor() { }
+  @Input() highlightText = ''
+  @Input() highlightColor = '#ff6'
+
+  originalHTML = ''
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['highlightText'].firstChange) {
+      this.originalHTML = this.elementRef.nativeElement.innerHTML
+      return
+    }
+    const { currentValue } = changes['highlightText']
+    if (currentValue) {
+      const regExp = new RegExp(`(${currentValue})`, 'gi')
+      this.elementRef.nativeElement.innerHTML = this.originalHTML.replace(
+        regExp,
+        `<span style="background-color: ${this.highlightColor}">\$1</span>`
+      )
+    } else {
+      this.elementRef.nativeElement.innerHTML = this.originalHTML
+    }
+    console.log(currentValue)
+  }
 
 }
