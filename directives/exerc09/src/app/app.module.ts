adc35e99fca65381583cd805004087cd06e10f04
paolomococci <paolomococci@gmail.com>
2022-05-23 07:37:41 +0200

    I have edited the app.module.ts file belonging to exercise exerc09

--- directives/exerc09/src/app/app.module.ts
+++ directives/exerc09/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighlightDirective } from './directives/highlight.directive'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
