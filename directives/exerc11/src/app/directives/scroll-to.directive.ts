3042e6f6676bd1531e019830b9275f9dde396870
paolomococci <paolomococci@gmail.com>
2022-05-24 18:03:10 +0200

    I have edited the scroll-to.directive.ts file belonging to exercise exerc11

--- directives/exerc11/src/app/directives/scroll-to.directive.ts
+++ directives/exerc11/src/app/directives/scroll-to.directive.ts
@@ -12,7 +12,8 @@ export class ScrollToDirective {
   @Input() target = ''
 
   @HostListener('click') onClick() {
-    console.log(this.target)
+    const targetElement = document.querySelector(this.target)
+    targetElement?.scrollIntoView()
   }
 
   constructor() { }
