816c6d6eb3853121a544ae24d608a413139ac1e5
paolomococci <paolomococci@gmail.com>
2022-05-24 17:23:40 +0200

    I have edited the app.component.ts file belonging to exercise exerc11

--- directives/exerc11/src/app/app.component.ts
+++ directives/exerc11/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'exerc11'
+  searchText = ''
+
+  makeRandomText(
+    lengthOfText: number,
+    charactersAccepted: string
+  ): string {
+    let randomText = ""
+    for (let i = 0; i < lengthOfText; i++) {
+      randomText += charactersAccepted.charAt(
+        Math.floor(Math.random() * charactersAccepted.length)
+      )
+    }
+    return randomText
+  }
+
+  randomText = this.makeRandomText(
+    3200,
+    "abcdefghijklmnopqrstuvwxyz_-ABCDEFGHIJKLMNOPQRSTUVWXYZ,.;:?!'\n\s"
+  )
 }
