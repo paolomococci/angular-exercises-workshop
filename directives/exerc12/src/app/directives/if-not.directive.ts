b788ceeebf696398ce56f899a85b9459f091f287
paolomococci <paolomococci@gmail.com>
2022-05-26 18:27:58 +0200

    I have edited the if-not.directive.ts file belonging to exercise exerc12

--- directives/exerc12/src/app/directives/if-not.directive.ts
+++ directives/exerc12/src/app/directives/if-not.directive.ts
@@ -11,6 +11,11 @@ import {
 export class IfNotDirective {
 
   @Input() set appIfNot(value: boolean) {
+    if (value === false) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef)
+    } else {
+      this.viewContainerRef.clear()
+    }
     console.log(`appIfNot value is: ${value}`)
   }
 
