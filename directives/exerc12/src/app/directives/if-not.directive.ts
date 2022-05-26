17358821b82ce288536204a8db3dafd39b92c3ef
paolomococci <paolomococci@gmail.com>
2022-05-26 18:19:58 +0200

    I have edited the if-not.directive.ts file belonging to exercise exerc12

--- directives/exerc12/src/app/directives/if-not.directive.ts
+++ directives/exerc12/src/app/directives/if-not.directive.ts
@@ -1,9 +1,14 @@
-import { Directive } from '@angular/core'
+import {
+  Directive,
+  Input
+} from '@angular/core'
 
 @Directive({
   selector: '[appIfNot]'
 })
 export class IfNotDirective {
+
+  @Input() set appIfNot(value: boolean) {}
 
   constructor() { }
 
