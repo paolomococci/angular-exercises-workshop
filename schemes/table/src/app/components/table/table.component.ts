d7a713ff7aa17fe9c4de59bde830bbdfcbdc9070
paolomococci <paolomococci@gmail.com>
2022-06-01 10:13:04 +0200

    I added file table.component.ts to scheme table

---
+++ schemes/table/src/app/components/table/table.component.ts
@@ -0,0 +1,29 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { MatTable } from '@angular/material/table'
+import { TableDataSource, TableItem } from './table-data-source'
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.sass']
+})
+export class TableComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<TableItem>;
+  dataSource: TableDataSource;
+
+  displayedColumns = ['id', 'name']
+
+  constructor() {
+    this.dataSource = new TableDataSource()
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
+    this.table.dataSource = this.dataSource
+  }
+}
