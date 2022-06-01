fba84a590538c9c17ff832836ad81bcb2d5bc836
paolomococci <paolomococci@gmail.com>
2022-06-01 10:12:49 +0200

    I added file table-data-source.ts to scheme table

---
+++ schemes/table/src/app/components/table/table-data-source.ts
@@ -0,0 +1,90 @@
+import { DataSource } from '@angular/cdk/collections'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { map } from 'rxjs/operators'
+import { Observable, of as observableOf, merge } from 'rxjs'
+
+// todo
+export interface TableItem {
+  name: string;
+  id: number;
+}
+
+// todo
+const EXAMPLE_DATA: TableItem[] = [
+  {id: 1, name: 'Hydrogen'},
+  {id: 2, name: 'Helium'},
+  {id: 3, name: 'Lithium'},
+  {id: 4, name: 'Beryllium'},
+  {id: 5, name: 'Boron'},
+  {id: 6, name: 'Carbon'},
+  {id: 7, name: 'Nitrogen'},
+  {id: 8, name: 'Oxygen'},
+  {id: 9, name: 'Fluorine'},
+  {id: 10, name: 'Neon'},
+  {id: 11, name: 'Sodium'},
+  {id: 12, name: 'Magnesium'},
+  {id: 13, name: 'Aluminum'},
+  {id: 14, name: 'Silicon'},
+  {id: 15, name: 'Phosphorus'},
+  {id: 16, name: 'Sulfur'},
+  {id: 17, name: 'Chlorine'},
+  {id: 18, name: 'Argon'},
+  {id: 19, name: 'Potassium'},
+  {id: 20, name: 'Calcium'},
+];
+
+export class TableDataSource extends DataSource<TableItem> {
+  data: TableItem[] = EXAMPLE_DATA;
+  paginator: MatPaginator | undefined;
+  sort: MatSort | undefined;
+
+  constructor() {
+    super()
+  }
+
+  connect(): Observable<TableItem[]> {
+    if (this.paginator && this.sort) {
+      // Combine everything that affects the rendered data into one update
+      // stream for the data-table to consume.
+      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
+        .pipe(map(() => {
+          return this.getPagedData(this.getSortedData([...this.data ]))
+        }))
+    } else {
+      throw Error('Please set the paginator and sort on the data source before connecting.')
+    }
+  }
+
+  disconnect(): void {}
+
+  private getPagedData(data: TableItem[]): TableItem[] {
+    if (this.paginator) {
+      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      return data.splice(startIndex, this.paginator.pageSize)
+    } else {
+      return data
+    }
+  }
+
+  private getSortedData(data: TableItem[]): TableItem[] {
+    if (!this.sort || !this.sort.active || this.sort.direction === '') {
+      return data
+    }
+
+    return data.sort((a, b) => {
+      const isAsc = this.sort?.direction === 'asc'
+      switch (this.sort?.active) {
+        case 'name': return compare(a.name, b.name, isAsc)
+        case 'id': return compare(+a.id, +b.id, isAsc)
+        default: return 0
+      }
+    });
+  }
+}
+
+function compare(
+  a: string | number, b: string | number, isAsc: boolean
+): number {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1)
+}
