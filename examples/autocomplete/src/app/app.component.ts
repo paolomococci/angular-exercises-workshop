818fee63c732d318d6708f0e3e226a750afdc473
paolomococci <paolomococci@gmail.com>
2022-06-03 18:35:27 +0200

    I have updated app.component.ts

--- examples/autocomplete/src/app/app.component.ts
+++ examples/autocomplete/src/app/app.component.ts
@@ -1,52 +1,12 @@
 import {
-  Component,
-  OnInit
+  Component
 } from '@angular/core'
-
-import { FormControl } from '@angular/forms'
-import {Observable} from 'rxjs'
-import {
-  map,
-  startWith
-} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'autocomplete'
-
-  control = new FormControl()
-
-  options: string[] = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine'
-  ]
-
-  filteredOptions!: Observable<string[]>
-
-  ngOnInit() {
-    this.filteredOptions = this.control.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value))
-    )
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase()
-    return this.options.filter(
-      option => option.toLowerCase().includes(
-        filterValue
-      )
-    )
-  }
 }
