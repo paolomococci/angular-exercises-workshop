0c1928abb6664f7ba50c53cd7b86d91502218936
paolomococci <paolomococci@gmail.com>
2022-06-03 16:45:27 +0200

    I have updated app.module.ts

--- examples/card/src/app/app.module.ts
+++ examples/card/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { CardComponent } from './components/card/card.component'
 
+import {MatCardModule} from '@angular/material/card'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { CardComponent } from './components/card/card.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
