5add87218ff308f25e15ec50ee59a860a9fbcaec
paolomococci <paolomococci@gmail.com>
2022-06-03 16:57:14 +0200

    I have updated card.component.ts

--- examples/card/src/app/components/card/card.component.ts
+++ examples/card/src/app/components/card/card.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core'
 })
 export class CardComponent implements OnInit {
 
+  sampleText: string = "Some text describing anything on this card."
+
   constructor() { }
 
   ngOnInit(): void {
