e85b704939d0add7e9ac91013729bc86e22996cf
paolomococci <paolomococci@gmail.com>
2022-06-04 07:57:05 +0200

    I have updated app.module.ts

--- examples/badge/src/app/app.module.ts
+++ examples/badge/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { SampleComponent } from './components/sample/sample.component'
 import { MatBadgeModule } from '@angular/material/badge'
 import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatBadgeModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
