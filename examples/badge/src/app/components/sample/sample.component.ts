10d6a0c9b998396ce9eb4b0571b37ab67ed957ac
paolomococci <paolomococci@gmail.com>
2022-06-04 07:57:32 +0200

    I have updated sample.component.ts

--- examples/badge/src/app/components/sample/sample.component.ts
+++ examples/badge/src/app/components/sample/sample.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core'
+import {DomSanitizer} from '@angular/platform-browser'
+import {MatIconRegistry} from '@angular/material/icon'
+
+const THUMB_UP_ICON =
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.51.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
+  </svg>
+  `
 
 @Component({
   selector: 'app-sample',
@@ -19,7 +29,15 @@ export class SampleComponent implements OnInit {
     }
   }
 
-  constructor() { }
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      'thumbs-up',
+      sanitizer.bypassSecurityTrustHtml(THUMB_UP_ICON)
+    )
+  }
 
   ngOnInit(): void {
   }
