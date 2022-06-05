f5a7ca8e223e1ca4ad30b90748b1c7d8687595ab
paolomococci <paolomococci@gmail.com>
2022-06-05 06:53:21 +0200

    I have updated kind.component.ts

--- examples/checkbox/src/app/components/kind/kind.component.ts
+++ examples/checkbox/src/app/components/kind/kind.component.ts
@@ -3,12 +3,20 @@ import {
   OnInit
 } from '@angular/core'
 
+import { Task } from 'src/app/models/task.model'
+
 @Component({
   selector: 'app-kind',
   templateUrl: './kind.component.html',
   styleUrls: ['./kind.component.sass']
 })
 export class KindComponent implements OnInit {
+
+  task: Task = {
+    name: 'unassigned',
+    completed: false,
+    color: undefined
+  }
 
   constructor() { }
 
