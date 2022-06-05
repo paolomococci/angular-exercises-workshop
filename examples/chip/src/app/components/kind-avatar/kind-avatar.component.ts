c926c6a5f46bbc388a41f6cfa388927372809381
paolomococci <paolomococci@gmail.com>
2022-06-05 17:36:18 +0200

    I added file kind-avatar.component.ts to example chip

---
+++ examples/chip/src/app/components/kind-avatar/kind-avatar.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-avatar',
+  templateUrl: './kind-avatar.component.html',
+  styleUrls: ['./kind-avatar.component.sass']
+})
+export class KindAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
