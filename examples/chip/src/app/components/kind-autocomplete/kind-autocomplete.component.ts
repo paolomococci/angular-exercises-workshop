9322e96fb7075fcf0957ed1e3265eaa3dee563c4
paolomococci <paolomococci@gmail.com>
2022-06-05 17:33:07 +0200

    I added file kind-autocomplete.component.ts to example chip

---
+++ examples/chip/src/app/components/kind-autocomplete/kind-autocomplete.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-autocomplete',
+  templateUrl: './kind-autocomplete.component.html',
+  styleUrls: ['./kind-autocomplete.component.sass']
+})
+export class KindAutocompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
