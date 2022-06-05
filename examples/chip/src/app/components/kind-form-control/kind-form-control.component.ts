8c4e16a40cd230d44061fddcebbfdc9a1d986c33
paolomococci <paolomococci@gmail.com>
2022-06-05 17:43:03 +0200

    I added file kind-form-control.component.ts to example chip

---
+++ examples/chip/src/app/components/kind-form-control/kind-form-control.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-form-control',
+  templateUrl: './kind-form-control.component.html',
+  styleUrls: ['./kind-form-control.component.sass']
+})
+export class KindFormControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
