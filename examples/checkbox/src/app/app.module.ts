408b4b537988f509b824cc904016e620144fb5ba
paolomococci <paolomococci@gmail.com>
2022-06-05 11:19:58 +0200

    I have updated app.module.ts

--- examples/checkbox/src/app/app.module.ts
+++ examples/checkbox/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { KindComponent } from './components/kind/kind.component'
 import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatListModule } from '@angular/material/list'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MatListModule } from '@angular/material/list'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
-    MatListModule
+    MatListModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
