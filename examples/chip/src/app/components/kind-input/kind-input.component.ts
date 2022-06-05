0978bd800c8959b7ec9d696fa0125092ae8353df
paolomococci <paolomococci@gmail.com>
2022-06-05 17:45:37 +0200

    I added file kind-input.component.ts to example chip

---
+++ examples/chip/src/app/components/kind-input/kind-input.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-input',
+  templateUrl: './kind-input.component.html',
+  styleUrls: ['./kind-input.component.sass']
+})
+export class KindInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
