594f1e8de4125d12d09a2cd31d0a94d92217244c
paolomococci <paolomococci@gmail.com>
2022-06-05 17:39:12 +0200

    I added file kind-dnd.component.ts to example chip

---
+++ examples/chip/src/app/components/kind-dnd/kind-dnd.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-dnd',
+  templateUrl: './kind-dnd.component.html',
+  styleUrls: ['./kind-dnd.component.sass']
+})
+export class KindDndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
