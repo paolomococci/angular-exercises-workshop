46103258bb0a62405472d3595111b601767574bf
paolomococci <paolomococci@gmail.com>
2022-06-05 12:23:11 +0200

    I have updated app.component.ts

--- examples/chip/src/app/components/kind/kind.component.ts
+++ examples/chip/src/app/components/kind/kind.component.ts
@@ -3,6 +3,9 @@ import {
   OnInit
 } from '@angular/core'
 
+import { FormControl } from '@angular/forms'
+import { MatChipInputEvent } from '@angular/material/chips'
+
 @Component({
   selector: 'app-kind',
   templateUrl: './kind.component.html',
