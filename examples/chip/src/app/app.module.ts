9bd25824698dd8657a16ca33f80f530ef1e69cb0
paolomococci <paolomococci@gmail.com>
2022-06-05 12:22:41 +0200

    I have updated app.module.ts

--- examples/chip/src/app/app.module.ts
+++ examples/chip/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { KindComponent } from './components/kind/kind.component'
+import { MatChipsModule } from '@angular/material/chips'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { KindComponent } from './components/kind/kind.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
