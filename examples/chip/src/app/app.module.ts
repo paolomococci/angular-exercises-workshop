9c13b8c4fb0773efa08e7dec1e7ac093664d56f1
paolomococci <paolomococci@gmail.com>
2022-06-06 22:23:25 +0200

    I have updated app.module.ts

--- examples/chip/src/app/app.module.ts
+++ examples/chip/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { KindFormControlComponent } from './components/kind-form-control/kind-fo
 import { KindInputComponent } from './components/kind-input/kind-input.component'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -30,7 +34,9 @@ import { MatIconModule } from '@angular/material/icon'
     BrowserAnimationsModule,
     MatChipsModule,
     MatFormFieldModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
