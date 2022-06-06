09f32f940983b2a8da837881943bcb433b399e87
paolomococci <paolomococci@gmail.com>
2022-06-06 18:21:11 +0200

    I added file kind-input.component.ts to example chip

--- examples/chip/src/app/components/kind-input/kind-input.component.ts
+++ examples/chip/src/app/components/kind-input/kind-input.component.ts
@@ -3,12 +3,28 @@ import {
   OnInit
  } from '@angular/core'
 
+ import { Fruit } from 'src/app/models/fruit.model'
+
+ import {
+  COMMA,
+  ENTER
+ } from '@angular/cdk/keycodes'
+
+ import { MatChipInputEvent } from '@angular/material/chips'
+
 @Component({
   selector: 'app-kind-input',
   templateUrl: './kind-input.component.html',
   styleUrls: ['./kind-input.component.sass']
 })
 export class KindInputComponent implements OnInit {
+
+  blur: boolean = true
+
+  readonly keyCodes = [
+    COMMA,
+    ENTER
+  ] as const
 
   constructor() { }
 
