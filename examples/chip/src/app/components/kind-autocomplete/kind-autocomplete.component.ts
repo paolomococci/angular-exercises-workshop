fd9ad0fd2632c991ce5865941766ba11cac854dd
paolomococci <paolomococci@gmail.com>
2022-06-07 22:02:13 +0200

    I have updated kind-autocomplete.component.ts

--- examples/chip/src/app/components/kind-autocomplete/kind-autocomplete.component.ts
+++ examples/chip/src/app/components/kind-autocomplete/kind-autocomplete.component.ts
@@ -1,7 +1,22 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  ElementRef,
+  ViewChild
  } from '@angular/core'
+
+ import {
+  COMMA,
+  ENTER
+ } from '@angular/cdk/keycodes'
+ import { FormControl } from '@angular/forms'
+ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete'
+ import { MatChipInputEvent } from '@angular/material/chips'
+ import { Observable } from 'rxjs'
+ import {
+  map,
+  startWith
+ } from 'rxjs/operators'
 
 @Component({
   selector: 'app-kind-autocomplete',
