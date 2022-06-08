ae491a710e6b79e479a2567e56f36e95bb825701
paolomococci <paolomococci@gmail.com>
2022-06-08 15:45:23 +0200

    I added file kind-date-filter-validation.ts to example datepicker

---
+++ examples/datepicker/src/app/components/kind-date-filter-validation/kind-date-filter-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-date-filter-validation',
+  templateUrl: './kind-date-filter-validation.component.html',
+  styleUrls: ['./kind-date-filter-validation.component.sass']
+})
+export class KindDateFilterValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
