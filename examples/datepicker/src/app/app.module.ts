6d2cf5d573ee93dce92738e569aee0f130de9432
paolomococci <paolomococci@gmail.com>
2022-06-08 15:43:42 +0200

    I have updated app.module.ts

--- examples/datepicker/src/app/app.module.ts
+++ examples/datepicker/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { KindComponent } from './components/kind/kind.component';
-import { KindDateRangePickerComponent } from './components/kind-date-range-picker/kind-date-range-picker.component'
+import { KindDateRangePickerComponent } from './components/kind-date-range-picker/kind-date-range-picker.component';
+import { KindDateFilterValidationComponent } from './components/kind-date-filter-validation/kind-date-filter-validation.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     KindComponent,
-    KindDateRangePickerComponent
+    KindDateRangePickerComponent,
+    KindDateFilterValidationComponent
   ],
   imports: [
     BrowserModule,
