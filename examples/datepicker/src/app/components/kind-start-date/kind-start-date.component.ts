6dbabd851d32a1901c3499e3a2e04695ae614f36
paolomococci <paolomococci@gmail.com>
2022-06-08 15:56:49 +0200

    I added file kind-start-date.ts to example datepicker

---
+++ examples/datepicker/src/app/components/kind-start-date/kind-start-date.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-start-date',
+  templateUrl: './kind-start-date.component.html',
+  styleUrls: ['./kind-start-date.component.sass']
+})
+export class KindStartDateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
