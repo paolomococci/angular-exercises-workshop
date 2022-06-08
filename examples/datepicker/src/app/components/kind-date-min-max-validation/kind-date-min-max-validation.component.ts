d6b5ce87e4a1fafffeb56834d2b76a4416259eef
paolomococci <paolomococci@gmail.com>
2022-06-08 15:50:29 +0200

    I added file kind-date-min-max-validation.ts to example datepicker

---
+++ examples/datepicker/src/app/components/kind-date-min-max-validation/kind-date-min-max-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-date-min-max-validation',
+  templateUrl: './kind-date-min-max-validation.component.html',
+  styleUrls: ['./kind-date-min-max-validation.component.sass']
+})
+export class KindDateMinMaxValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
