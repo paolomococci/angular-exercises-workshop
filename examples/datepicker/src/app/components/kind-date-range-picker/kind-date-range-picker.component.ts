b8e9cd365470a7596411d30f02f5f115bbfc26dc
paolomococci <paolomococci@gmail.com>
2022-06-08 15:37:11 +0200

    I added file kind-date-range-picker.component.ts to example datepicker

---
+++ examples/datepicker/src/app/components/kind-date-range-picker/kind-date-range-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-date-range-picker',
+  templateUrl: './kind-date-range-picker.component.html',
+  styleUrls: ['./kind-date-range-picker.component.sass']
+})
+export class KindDateRangePickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
