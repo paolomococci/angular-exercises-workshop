75828a0fe46bacb239919c0a3c4dea209acc9dc1
paolomococci <paolomococci@gmail.com>
2022-06-12 13:00:54 +0200

    I have updated app-routing.module.ts

--- examples/chip/src/app/app-routing.module.ts
+++ examples/chip/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { KindAutocompleteComponent } from './components/kind-autocomplete/kind-autocomplete.component'
 import { KindAvatarComponent } from './components/kind-avatar/kind-avatar.component'
+import { KindDndComponent } from './components/kind-dnd/kind-dnd.component'
 import { KindComponent } from './components/kind/kind.component'
 
 const routes: Routes = [
@@ -22,11 +23,11 @@ const routes: Routes = [
     path: 'kind-avatar',
     component: KindAvatarComponent
   },
-  /*{
-    path: 'kind-dnd',
-    component:
-  },
   {
+    path: 'kind-dnd',
+    component: KindDndComponent
+  },
+  /*{
     path: 'kind-form-control',
     component:
   },
