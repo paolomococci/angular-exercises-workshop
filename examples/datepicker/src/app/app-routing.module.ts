84b90af707df978eb1e7d169cc1abb14ee53f0da
paolomococci <paolomococci@gmail.com>
2022-06-12 12:19:44 +0200

    I have updated app-routing.module.ts

--- examples/datepicker/src/app/app-routing.module.ts
+++ examples/datepicker/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+import { KindComponent } from './components/kind/kind.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/kind',
+    pathMatch: 'full'
+  },
+  {
+    path: 'kind',
+    component: KindComponent
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
