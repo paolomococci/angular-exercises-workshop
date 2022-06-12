22d52ca07c4dca3e07a18a33e69aac219e7ba5dd
paolomococci <paolomococci@gmail.com>
2022-06-12 13:03:45 +0200

    I have updated app-routing.module.ts

--- examples/chip/src/app/app-routing.module.ts
+++ examples/chip/src/app/app-routing.module.ts
@@ -37,6 +37,11 @@ const routes: Routes = [
     path: 'kind-input',
     component: KindInputComponent
   },
+  {
+    path: '**',
+    redirectTo: '/kind',
+    pathMatch: 'full'
+  },
 ]
 
 @NgModule({
