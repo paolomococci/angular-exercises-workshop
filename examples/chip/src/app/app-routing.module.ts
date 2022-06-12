55df3fdcf9a9a606835ac8c40257dac335b4241f
paolomococci <paolomococci@gmail.com>
2022-06-12 12:56:23 +0200

    I have updated app-routing.module.ts

--- examples/chip/src/app/app-routing.module.ts
+++ examples/chip/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { KindComponent } from './components/kind/kind.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/kind',
+    pathMatch: 'full'
+  },
+  {
+    path: 'kind',
+    component: KindComponent
+  },
+  /*{
+    path: 'kind-autocomplete',
+    component:
+  },
+  {
+    path: 'kind-avatar',
+    component:
+  },
+  {
+    path: 'kind-dnd',
+    component:
+  },
+  {
+    path: 'kind-form-control',
+    component:
+  },
+  {
+    path: 'kind-input',
+    component:
+  },*/
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
