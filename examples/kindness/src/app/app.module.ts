3d190b5be979a387f8a464de0e69d9eeda51f159
paolomococci <paolomococci@gmail.com>
2022-06-13 06:41:16 +0200

    I have updated app.module.ts

--- examples/kindness/src/app/app.module.ts
+++ examples/kindness/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { KindComponent } from './components/kind/kind.component'
+import { MatCardModule } from '@angular/material/card'
 import { MatSidenavModule } from '@angular/material/sidenav'
 
 @NgModule({
@@ -16,6 +17,7 @@ import { MatSidenavModule } from '@angular/material/sidenav'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatCardModule,
     MatSidenavModule
   ],
   providers: [],
