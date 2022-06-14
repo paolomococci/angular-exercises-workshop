dc9ebb80c0c698b0552179e287b7c28f27807aaa
paolomococci <paolomococci@gmail.com>
2022-06-14 13:02:10 +0200

    I have updated app.module.ts

--- examples/fourteen/src/app/app.module.ts
+++ examples/fourteen/src/app/app.module.ts
@@ -7,18 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { KindComponent } from './components/kind/kind.component'
 import { MatCardModule } from '@angular/material/card'
 import { MatSidenavModule } from '@angular/material/sidenav'
+import { NavigationBarComponent } from '../app/components/navigation-bar/navigation-bar.component'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
-    KindComponent
+    KindComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatSidenavModule
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
