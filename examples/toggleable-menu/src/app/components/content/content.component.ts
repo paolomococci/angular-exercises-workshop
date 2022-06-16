0a3243f538ca7c4d4e4e8c496098e6614341c77a
paolomococci <paolomococci@gmail.com>
2022-06-16 14:37:30 +0200

    I added file content.component.ts to example toggleable-menu

---
+++ examples/toggleable-menu/src/app/components/content/content.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.sass']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
