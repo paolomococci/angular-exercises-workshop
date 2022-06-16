fed9c8ce15e349908ee97264fec27a2fb8b1039b
paolomococci <paolomococci@gmail.com>
2022-06-16 16:22:29 +0200

    I added file content.component.ts to example static-side-menu

---
+++ examples/static-side-menu/src/app/components/content/content.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core'
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.sass']
+})
+export class ContentComponent implements OnInit {
+
+  @Input()
+  title!: string
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
