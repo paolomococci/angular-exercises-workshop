11e7cd6a0709523f9a4d5f963407ce84534aa469
paolomococci <paolomococci@gmail.com>
2022-06-16 15:18:58 +0200

    I have updated drop-right-menu.component.ts

--- examples/toggleable-menu/src/app/components/drop-right-menu/drop-right-menu.component.ts
+++ examples/toggleable-menu/src/app/components/drop-right-menu/drop-right-menu.component.ts
@@ -2,6 +2,15 @@ import {
   Component,
   OnInit
 } from '@angular/core'
+import {
+  BreakpointObserver,
+  Breakpoints
+} from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import {
+  map,
+  shareReplay
+ } from 'rxjs/operators'
 
 @Component({
   selector: 'app-drop-right-menu',
@@ -10,7 +19,18 @@ import {
 })
 export class DropRightMenuComponent implements OnInit {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(
+      Breakpoints.Handset
+    )
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    )
+
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) { }
 
   ngOnInit(): void {
   }
