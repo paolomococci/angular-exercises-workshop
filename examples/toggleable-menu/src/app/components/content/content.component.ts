63f3f385fa7912cffe874da262729e184f7ec8bc
paolomococci <paolomococci@gmail.com>
2022-06-16 15:00:06 +0200

    I have updated content.component.ts

--- examples/toggleable-menu/src/app/components/content/content.component.ts
+++ examples/toggleable-menu/src/app/components/content/content.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  Input
 } from '@angular/core'
 
 @Component({
@@ -9,6 +10,9 @@ import {
   styleUrls: ['./content.component.sass']
 })
 export class ContentComponent implements OnInit {
+
+  @Input()
+  title!: string
 
   constructor() { }
 
