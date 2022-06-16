7f6b0d6951aa5c619626a299e6f43e77046a8c53
paolomococci <paolomococci@gmail.com>
2022-06-16 14:28:28 +0200

    I added file drop-right-menu.component.ts to example toggleable-menu

---
+++ examples/toggleable-menu/src/app/components/drop-right-menu/drop-right-menu.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-drop-right-menu',
+  templateUrl: './drop-right-menu.component.html',
+  styleUrls: ['./drop-right-menu.component.sass']
+})
+export class DropRightMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
