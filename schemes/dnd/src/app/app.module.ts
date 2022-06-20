02fd9a83885c644f459b7f3464bb22764f16e43d
paolomococci <paolomococci@gmail.com>
2022-06-20 10:08:19 +0200

    I have updated app.module.ts

--- schemes/dnd/src/app/app.module.ts
+++ schemes/dnd/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatIconModule } from '@angular/material/icon'
 import { DragDropModule } from '@angular/cdk/drag-drop'
 
 import { DndComponent } from './components/dnd/dnd.component'
@@ -19,6 +20,7 @@ import { MainContentComponent } from './components/main-content/main-content.com
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatIconModule,
     DragDropModule
   ],
   providers: [],
