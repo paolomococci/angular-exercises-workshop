dd949f8b11d5747f5f5a889873263dd4d908243a
paolomococci <paolomococci@gmail.com>
2022-06-21 08:14:20 +0200

    I have updated app.module.ts

--- schemes/table/src/app/app.module.ts
+++ schemes/table/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { TableComponent } from './components/table/table.component'
 import { MatTableModule } from '@angular/material/table'
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSortModule } from '@angular/material/sort'
 
+import { MainContentComponent } from './components/main-content/main-content.component'
+import { TableComponent } from './components/table/table.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
