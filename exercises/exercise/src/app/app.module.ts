4c0a2ecc2a96d921e491278968a123b5a466778f
paolomococci <paolomococci@gmail.com>
2022-06-22 09:47:32 +0200

    I have updated app.module.ts

--- exercises/exercise/src/app/app.module.ts
+++ exercises/exercise/src/app/app.module.ts
@@ -9,16 +9,33 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { MatCardModule } from '@angular/material/card'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list';
+import { PracticeComponent } from './components/practice/practice.component'
+
 @NgModule({
   declarations: [
     AppComponent,
     RollerBlindMenuComponent,
-    MainContentComponent
+    MainContentComponent,
+    PracticeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
