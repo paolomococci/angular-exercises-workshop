da30db694f4c09afbb3224c2bb58dc03082f584f
paolomococci <paolomococci@gmail.com>
2022-06-22 08:42:16 +0200

    I added file roller-blind-menu.component.ts to web app exercise

---
+++ exercises/exercise/src/app/components/roller-blind-menu/roller-blind-menu.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-roller-blind-menu',
+  templateUrl: './roller-blind-menu.component.html',
+  styleUrls: ['./roller-blind-menu.component.sass']
+})
+export class RollerBlindMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
