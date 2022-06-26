83c4cb2c706fda78228dfd5c4ce104c6ff5754c8
paolomococci <paolomococci@gmail.com>
2022-06-26 19:45:22 +0200

    I added file roller-blind-menu.component.ts to web app template named exercise

---
+++ exercises/exercise/src/app/components/roller-blind-menu/roller-blind-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-roller-blind-menu',
+  templateUrl: './roller-blind-menu.component.html',
+  styleUrls: ['./roller-blind-menu.component.css']
+})
+export class RollerBlindMenuComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
