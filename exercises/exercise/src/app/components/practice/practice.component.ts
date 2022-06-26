1d27cd4f0db0fd01ca81036e3c9a0971cd6791d6
paolomococci <paolomococci@gmail.com>
2022-06-26 20:01:35 +0200

    I added file practice.component.ts to web app template named exercise

---
+++ exercises/exercise/src/app/components/practice/practice.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-practice',
+  templateUrl: './practice.component.html',
+  styleUrls: ['./practice.component.sass']
+})
+export class PracticeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
