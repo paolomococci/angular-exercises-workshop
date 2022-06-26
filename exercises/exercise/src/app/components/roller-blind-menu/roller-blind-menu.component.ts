3b00c789ff695b4c6db69b0c8363e5e55f7659de
paolomococci <paolomococci@gmail.com>
2022-06-26 19:55:21 +0200

    I have updated roller-blind-menu.component.ts

--- exercises/exercise/src/app/components/roller-blind-menu/roller-blind-menu.component.ts
+++ exercises/exercise/src/app/components/roller-blind-menu/roller-blind-menu.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from '@angular/core'
+import {
+  BreakpointObserver,
+  Breakpoints
+} from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import {
+  map,
+  shareReplay
+} from 'rxjs/operators'
 
 @Component({
   selector: 'app-roller-blind-menu',
   templateUrl: './roller-blind-menu.component.html',
-  styleUrls: ['./roller-blind-menu.component.css']
+  styleUrls: ['./roller-blind-menu.component.sass']
 })
 export class RollerBlindMenuComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map(
+        result => result.matches
+      ),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
 }
