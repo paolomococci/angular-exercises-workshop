028f20e29bff6d8fdaa5d27c35d7b80a16826db1
paolomococci <paolomococci@gmail.com>
2022-06-27 23:26:55 +0200

    I have updated app-routing.module.ts

--- exercises/exercise/src/app/app-routing.module.ts
+++ exercises/exercise/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { PracticeComponent } from './components/practice/practice.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/practice',
+    pathMatch: 'full'
+  },
+  {
+    path: 'kind',
+    component: PracticeComponent
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
