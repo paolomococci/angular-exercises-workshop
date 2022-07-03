ff0058cd65b0043cce0e2604fe3d819b1c596a58
paolomococci <paolomococci@gmail.com>
2022-07-03 17:34:10 +0200

    I have updated navbar.component.ts

--- examples/datepicker/src/app/components/navbar/navbar.component.ts
+++ examples/datepicker/src/app/components/navbar/navbar.component.ts
@@ -4,6 +4,20 @@ import {
   Input
 } from '@angular/core'
 
+import {
+  BreakpointObserver,
+  Breakpoints
+} from '@angular/cdk/layout'
+
+import {
+  Observable
+} from 'rxjs'
+
+import {
+  map,
+  shareReplay
+ } from 'rxjs/operators'
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
