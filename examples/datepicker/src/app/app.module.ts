3b12485b1b07805567c860a6b48af7b6c728b772
paolomococci <paolomococci@gmail.com>
2022-07-03 17:48:31 +0200

    I have updated app.module.ts

--- examples/datepicker/src/app/app.module.ts
+++ examples/datepicker/src/app/app.module.ts
@@ -4,18 +4,26 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 import { KindComponent } from './components/kind/kind.component'
 import { KindDateRangePickerComponent } from './components/kind-date-range-picker/kind-date-range-picker.component'
 import { KindDateFilterValidationComponent } from './components/kind-date-filter-validation/kind-date-filter-validation.component'
 import { KindDateMinMaxValidationComponent } from './components/kind-date-min-max-validation/kind-date-min-max-validation.component'
 import { MainComponent } from './components/main/main.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
-
 import { KindStartDateComponent } from './components/kind-start-date/kind-start-date.component'
+
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatNativeDateModule } from '@angular/material/core'
 import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
@@ -35,7 +43,14 @@ import { MatInputModule } from '@angular/material/input'
     MatDatepickerModule,
     MatFormFieldModule,
     MatNativeDateModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
