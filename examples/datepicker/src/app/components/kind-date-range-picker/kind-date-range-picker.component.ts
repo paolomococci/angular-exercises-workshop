335df59a2f490aa414e3e66f3b1a6f14adfcc00c
paolomococci <paolomococci@gmail.com>
2022-07-03 19:09:18 +0200

    I have updated kind-date-range-picker.component.ts

--- examples/datepicker/src/app/components/kind-date-range-picker/kind-date-range-picker.component.ts
+++ examples/datepicker/src/app/components/kind-date-range-picker/kind-date-range-picker.component.ts
@@ -3,12 +3,22 @@ import {
   OnInit
 } from '@angular/core'
 
+import {
+  FormGroup,
+  FormControl
+} from '@angular/forms'
+
 @Component({
   selector: 'app-kind-date-range-picker',
   templateUrl: './kind-date-range-picker.component.html',
   styleUrls: ['./kind-date-range-picker.component.sass']
 })
 export class KindDateRangePickerComponent implements OnInit {
+
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null)
+  })
 
   constructor() { }
 
