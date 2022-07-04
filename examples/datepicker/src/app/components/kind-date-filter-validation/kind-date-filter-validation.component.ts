da822c6dc0a331734da3ccae34466929a670593b
paolomococci <paolomococci@gmail.com>
2022-07-04 07:00:09 +0200

    I have updated kind-date-filter-validation.component.ts

--- examples/datepicker/src/app/components/kind-date-filter-validation/kind-date-filter-validation.component.ts
+++ examples/datepicker/src/app/components/kind-date-filter-validation/kind-date-filter-validation.component.ts
@@ -10,6 +10,11 @@ import {
 })
 export class KindDateFilterValidationComponent implements OnInit {
 
+  weekday = (date: Date | null): boolean => {
+    const day = (date || new Date()).getDay()
+    return day !== 0 && day !== 6
+  }
+
   constructor() { }
 
   ngOnInit(): void {
