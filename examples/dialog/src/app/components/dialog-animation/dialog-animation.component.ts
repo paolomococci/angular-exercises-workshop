1e96c3e5bae85111cb870c729500d5a610c6ca56
paolomococci <paolomococci@gmail.com>
2022-07-09 16:28:47 +0200

    new file:   examples/dialog/src/app/components/dialog-animation/dialog-animation.component.ts

---
+++ examples/dialog/src/app/components/dialog-animation/dialog-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-animation',
+  templateUrl: './dialog-animation.component.html',
+  styleUrls: ['./dialog-animation.component.sass']
+})
+export class DialogAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
