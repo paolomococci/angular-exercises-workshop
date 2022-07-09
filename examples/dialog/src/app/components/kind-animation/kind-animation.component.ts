2f3829a5fc9b54d462691487eceb08a7d6714aee
paolomococci <paolomococci@gmail.com>
2022-07-09 16:29:22 +0200

    new file:   examples/dialog/src/app/components/kind-animation/kind-animation.component.ts

---
+++ examples/dialog/src/app/components/kind-animation/kind-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-animation',
+  templateUrl: './kind-animation.component.html',
+  styleUrls: ['./kind-animation.component.sass']
+})
+export class KindAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
