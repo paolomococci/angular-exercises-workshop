92e693e95833a3487fafbc0a87e4babdd7e55243
paolomococci <paolomococci@gmail.com>
2022-07-12 07:02:38 +0200

    modified:   examples/web-editor/src/app/app.module.ts

--- examples/web-editor/src/app/app.module.ts
+++ examples/web-editor/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { MainComponent } from './components/main/main.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
