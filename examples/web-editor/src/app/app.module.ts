cbfe4ac08cedaf0c0e0a5e9c2478657276bd4305
paolomococci <paolomococci@gmail.com>
2022-07-12 07:07:55 +0200

    modified:   examples/web-editor/src/app/app.module.ts

--- examples/web-editor/src/app/app.module.ts
+++ examples/web-editor/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
+
 import { MainComponent } from './components/main/main.component'
+import { KindComponent } from './components/kind/kind.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    KindComponent
   ],
   imports: [
     BrowserModule
