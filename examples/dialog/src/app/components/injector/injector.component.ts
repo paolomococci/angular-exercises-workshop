55b93b29eea6749538bd361c060b742f3ed7760b
paolomococci <paolomococci@gmail.com>
2022-07-13 06:56:07 +0200

    new file:   examples/dialog/src/app/components/injector/injector.component.ts

---
+++ examples/dialog/src/app/components/injector/injector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-injector',
+  templateUrl: './injector.component.html',
+  styleUrls: ['./injector.component.sass']
+})
+export class InjectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
