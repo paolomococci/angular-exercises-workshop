9441e65d95687255f25c11de0f9f943a1346e88a
paolomococci <paolomococci@gmail.com>
2022-07-13 07:00:13 +0200

    new file:   examples/dialog/src/app/components/injected-data-dialog/injected-data-dialog.component.ts

---
+++ examples/dialog/src/app/components/injected-data-dialog/injected-data-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-injected-data-dialog',
+  templateUrl: './injected-data-dialog.component.html',
+  styleUrls: ['./injected-data-dialog.component.sass']
+})
+export class InjectedDataDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
