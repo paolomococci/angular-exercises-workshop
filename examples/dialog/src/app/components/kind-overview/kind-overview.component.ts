953fdca34ff3e9573ff0c227e005d54cdf97f695
paolomococci <paolomococci@gmail.com>
2022-07-14 12:19:35 +0200

    new file:   examples/dialog/src/app/components/kind-overview/kind-overview.component.ts

---
+++ examples/dialog/src/app/components/kind-overview/kind-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-overview',
+  templateUrl: './kind-overview.component.html',
+  styleUrls: ['./kind-overview.component.sass']
+})
+export class KindOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
