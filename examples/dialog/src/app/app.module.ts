74cfe9200c3eedaf55176877a012845c33967ea1
paolomococci <paolomococci@gmail.com>
2022-07-15 09:19:44 +0200

    modified:   examples/dialog/src/app/app.module.ts

--- examples/dialog/src/app/app.module.ts
+++ examples/dialog/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatDialogModule } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { MatDialogModule } from '@angular/material/dialog'
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
