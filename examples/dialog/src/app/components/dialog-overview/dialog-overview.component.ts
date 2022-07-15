64fbb1d58be95f55fd753bd23f364dffb462aac2
paolomococci <paolomococci@gmail.com>
2022-07-15 08:54:29 +0200

    modified:   examples/dialog/src/app/components/dialog-overview/dialog-overview.component.ts

--- examples/dialog/src/app/components/dialog-overview/dialog-overview.component.ts
+++ examples/dialog/src/app/components/dialog-overview/dialog-overview.component.ts
@@ -3,6 +3,11 @@ import {
   OnInit
 } from '@angular/core'
 
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog'
+
 @Component({
   selector: 'app-dialog-overview',
   templateUrl: './dialog-overview.component.html',
