204ba66e158d3540238e158810180faf3163966a
paolomococci <paolomococci@gmail.com>
2022-07-16 07:32:16 +0200

    modified:   examples/divider/src/app/app.module.ts

--- examples/divider/src/app/app.module.ts
+++ examples/divider/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
+import { MatDividerModule } from '@angular/material/divider'
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { MatListModule } from '@angular/material/list'
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
