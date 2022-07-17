def2294ba91514228de4599b5d9d9c8d17225380
paolomococci <paolomococci@gmail.com>
2022-07-17 05:48:14 +0200

    modified:   examples/panel/src/app/app.module.ts

--- examples/panel/src/app/app.module.ts
+++ examples/panel/src/app/app.module.ts
@@ -4,7 +4,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FormsModule } from '@angular/forms'
+
+import {
+  FormsModule,
+  ReactiveFormsModule
+ } from '@angular/forms'
 
 import { MainComponent } from './components/main/main.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
@@ -34,6 +38,7 @@ import { MatNativeDateModule } from '@angular/material/core'
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     MatCardModule,
     MatSidenavModule,
     LayoutModule,
