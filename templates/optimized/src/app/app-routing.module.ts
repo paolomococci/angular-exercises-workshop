69d2e8b26da9ae549d18ebcb57c7439f1e4c1502
paolomococci <paolomococci@gmail.com>
2022-07-19 20:20:32 +0200

    modified:   templates/optimized/src/app/app-routing.module.ts

--- templates/optimized/src/app/app-routing.module.ts
+++ templates/optimized/src/app/app-routing.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core'
+
 import {
   RouterModule,
   Routes
  } from '@angular/router'
 
-const routes: Routes = []
+import {
+  VarietyComponent
+ } from './components/variety/variety.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/variety',
+    pathMatch: 'full'
+  },
+  {
+    path: 'variety',
+    component: VarietyComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/variety',
+    pathMatch: 'full'
+  },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
