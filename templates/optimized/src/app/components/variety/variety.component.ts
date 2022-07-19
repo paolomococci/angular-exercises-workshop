acf2055d2594f4df678a9275a1a32266accee939
paolomococci <paolomococci@gmail.com>
2022-07-19 19:43:17 +0200

    new file:   templates/optimized/src/app/components/variety/variety.component.ts

---
+++ templates/optimized/src/app/components/variety/variety.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-variety',
+  templateUrl: './variety.component.html',
+  styleUrls: ['./variety.component.sass']
+})
+export class VarietyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
