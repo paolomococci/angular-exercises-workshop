ac53297740335c0e7dafb631d8670921e0179986
paolomococci <paolomococci@gmail.com>
2022-07-21 20:18:24 +0200

    modified:   examples/variety/src/app/app-routing.module.ts

--- examples/variety/src/app/app-routing.module.ts
+++ examples/variety/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+
+import {
+  RouterModule,
+  Routes
+} from '@angular/router'
+
+import {
+  VarietyComponent
+} from './components/variety/variety.component'
 
 const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: '/variety',
+    pathMatch: 'full'
+  },
+  {
+    path: 'variety',
+    component: VarietyComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/variety',
+    pathMatch: 'full'
+  },
 ]
 
 @NgModule({
