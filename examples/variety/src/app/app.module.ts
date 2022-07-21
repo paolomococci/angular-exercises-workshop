77d653356be6942f0d7184927d031658a7f4be46
paolomococci <paolomococci@gmail.com>
2022-07-21 18:53:45 +0200

    modified:   examples/variety/src/app/app.module.ts

--- examples/variety/src/app/app.module.ts
+++ examples/variety/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { MainComponent } from './components/main/main.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
