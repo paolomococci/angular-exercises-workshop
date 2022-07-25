4e0205b68c39cb39dd12d98845311cf80f5885d0
paolomococci <paolomococci@gmail.com>
2022-07-25 07:14:36 +0200

    new file:   examples/grid-list/src/app/app.module.ts

---
+++ examples/grid-list/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MainComponent } from './components/main/main.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { KindComponent } from './components/kind/kind.component'
+
+import { MatCardModule } from '@angular/material/card'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    KindComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
