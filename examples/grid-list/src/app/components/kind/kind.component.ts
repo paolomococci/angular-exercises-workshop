18aac26bbff91f1cc7fe0063cbc5ba5274aca440
paolomococci <paolomococci@gmail.com>
2022-07-26 13:31:02 +0200

    modified:   examples/grid-list/src/app/components/kind/kind.component.ts

--- examples/grid-list/src/app/components/kind/kind.component.ts
+++ examples/grid-list/src/app/components/kind/kind.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit
 } from '@angular/core'
+import { Tile } from 'src/app/models/tile.model'
 
 @Component({
   selector: 'app-kind',
@@ -9,6 +10,33 @@ import {
   styleUrls: ['./kind.component.sass']
 })
 export class KindComponent implements OnInit {
+
+  tiles : Tile[] = [
+    {
+      color: '#fbb',
+      columns: 3,
+      rows: 1,
+      text: '1st'
+    },
+    {
+      color: '#bbd',
+      columns: 1,
+      rows: 2,
+      text: '2nd'
+    },
+    {
+      color: '#adf',
+      columns: 1,
+      rows: 1,
+      text: '3rd'
+    },
+    {
+      color: '#deb',
+      columns: 2,
+      rows: 1,
+      text: '4th'
+    }
+  ]
 
   constructor() { }
 
