aa92be7de53cbbec77fb8b6c986220a79efd52e4
paolomococci <paolomococci@gmail.com>
2022-07-30 12:05:38 +0200

    new file:   examples/menu/src/app/components/kind-positioning/kind-positioning.component.ts

---
+++ examples/menu/src/app/components/kind-positioning/kind-positioning.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-positioning',
+  templateUrl: './kind-positioning.component.html',
+  styleUrls: ['./kind-positioning.component.sass']
+})
+export class KindPositioningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
