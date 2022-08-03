c37c63f9770e171520a2a6e06c631e6b809665ba
paolomococci <paolomococci@gmail.com>
2022-08-03 18:02:29 +0200

    new file:   examples/select/src/app/components/kind-validation/kind-validation.component.ts

---
+++ examples/select/src/app/components/kind-validation/kind-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-validation',
+  templateUrl: './kind-validation.component.html',
+  styleUrls: ['./kind-validation.component.sass']
+})
+export class KindValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
