e0554d6c7eb53dc9cc4647b8396f6235bba966b1
paolomococci <paolomococci@gmail.com>
2022-08-03 16:51:49 +0200

    modified:   examples/select/src/app/components/kind-disabled/kind-disabled.component.ts

--- examples/select/src/app/components/kind-disabled/kind-disabled.component.ts
+++ examples/select/src/app/components/kind-disabled/kind-disabled.component.ts
@@ -3,7 +3,7 @@ import {
   OnInit
  } from '@angular/core'
 
- import { FormControl } from '@angular/forms'
+ import { Fruit } from 'src/app/models/fruit.model'
 
 @Component({
   selector: 'app-kind-disabled',
@@ -11,6 +11,17 @@ import {
   styleUrls: ['./kind-disabled.component.sass']
 })
 export class KindDisabledComponent implements OnInit {
+
+  fruits: Fruit[] = [
+    {value: '11001', viewValue: 'apple'},
+    {value: '11002', viewValue: 'grape'},
+    {value: '11003', viewValue: 'orange'},
+    {value: '11004', viewValue: 'pear'},
+    {value: '11005', viewValue: 'banana'},
+    {value: '11006', viewValue: 'mango'},
+    {value: '11007', viewValue: 'avocado'},
+    {value: '11008', viewValue: 'pineapple'}
+  ]
 
   constructor() { }
 
