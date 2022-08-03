8b90bc75b401cd8eedcdd93d40ea2d0bd6911edb
paolomococci <paolomococci@gmail.com>
2022-08-03 11:07:43 +0200

    new file:   examples/select/src/app/components/kind-disabled/kind-disabled.component.ts

---
+++ examples/select/src/app/components/kind-disabled/kind-disabled.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-disabled',
+  templateUrl: './kind-disabled.component.html',
+  styleUrls: ['./kind-disabled.component.sass']
+})
+export class KindDisabledComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
