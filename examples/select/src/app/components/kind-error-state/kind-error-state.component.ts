5f86594504e01c36a894c5c4f2f8a19ea03bed25
paolomococci <paolomococci@gmail.com>
2022-08-03 11:10:50 +0200

    new file:   examples/select/src/app/components/kind-error-state/kind-error-state.component.ts

---
+++ examples/select/src/app/components/kind-error-state/kind-error-state.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-error-state',
+  templateUrl: './kind-error-state.component.html',
+  styleUrls: ['./kind-error-state.component.sass']
+})
+export class KindErrorStateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
