2f39bc532f3baa828655f66d5de3b20054d49d54
paolomococci <paolomococci@gmail.com>
2022-08-03 18:11:37 +0200

    modified:   examples/select/src/app/components/kind-validation/kind-validation.component.ts

--- examples/select/src/app/components/kind-validation/kind-validation.component.ts
+++ examples/select/src/app/components/kind-validation/kind-validation.component.ts
@@ -3,6 +3,8 @@ import {
   OnInit
  } from '@angular/core'
 
+ import { Fruit } from 'src/app/models/fruit.model'
+
 import {
   FormControl,
   Validators
@@ -14,6 +16,17 @@ import {
   styleUrls: ['./kind-validation.component.sass']
 })
 export class KindValidationComponent implements OnInit {
+
+  fruits: Fruit[] = [
+    {value: '11001', viewValue: 'apple'},
+    {value: '11002', viewValue: 'grape'},
+    {value: '11003', viewValue: 'orange'},
+    {value: '11004', viewValue: 'pear'},
+    {value: '11005', viewValue: 'banana'},
+    {value: '11006', viewValue: 'mango'},
+    {value: '11007', viewValue: 'avocado'},
+    {value: '11008', viewValue: 'pineapple'}
+  ]
 
   constructor() { }
 
