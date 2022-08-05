4f45fdfdf047a6e0896aa467a8193ea94ae110d5
paolomococci <paolomococci@gmail.com>
2022-08-05 09:57:23 +0200

    new file:   examples/slide-toggle/src/app/components/kind-form/kind-form.component.ts

---
+++ examples/slide-toggle/src/app/components/kind-form/kind-form.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+ } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-form',
+  templateUrl: './kind-form.component.html',
+  styleUrls: ['./kind-form.component.sass']
+})
+export class KindFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
