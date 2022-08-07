4fbae8442e0029d1d3ab10924d76e12e120a29ac
paolomococci <paolomococci@gmail.com>
2022-08-07 13:55:36 +0200

    modified:   examples/snack-bar/src/app/components/kind/kind.component.ts

--- examples/snack-bar/src/app/components/kind/kind.component.ts
+++ examples/snack-bar/src/app/components/kind/kind.component.ts
@@ -3,6 +3,8 @@ import {
   OnInit
 } from '@angular/core'
 
+import { MatSnackBar } from '@angular/material/snack-bar'
+
 @Component({
   selector: 'app-kind',
   templateUrl: './kind.component.html',
