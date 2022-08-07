f9cf14b5a85bea1efdd369ca92f6277e57585ca3
paolomococci <paolomococci@gmail.com>
2022-08-07 16:08:58 +0200

    modified:   examples/sort-header/src/app/components/kind/kind.component.ts

--- examples/sort-header/src/app/components/kind/kind.component.ts
+++ examples/sort-header/src/app/components/kind/kind.component.ts
@@ -18,7 +18,7 @@ export class KindComponent implements OnInit {
     {name: "apricot", water: 91.2, carbohydrates: 6.7, proteins: 0.4, fat: 0.09, fiber: 1.61},
     {name: "pineapple", water: 97.4, carbohydrates: 0.9, proteins: 0.6,  fat: 0.0, fiber: 1.1},
     {name: "avocado", water: 64, carbohydrates: 2.1, proteins: 6.2,  fat: 24.2, fiber: 3.5},
-    {name: "", water: null, carbohydrates: null, proteins: null,  fat: null, fiber: null},
+    {name: "orange", water: 89.1, carbohydrates: 8.2, proteins: 0.6,  fat: 0.3, fiber: 1.8},
     {name: "", water: null, carbohydrates: null, proteins: null,  fat: null, fiber: null},
     {name: "", water: null, carbohydrates: null, proteins: null,  fat: null, fiber: null},
     {name: "", water: null, carbohydrates: null, proteins: null,  fat: null, fiber: null},
