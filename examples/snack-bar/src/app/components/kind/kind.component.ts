c556b06607cb45826fd6b3741ae4d6fcf8edc1e5
paolomococci <paolomococci@gmail.com>
2022-08-07 14:26:57 +0200

    modified:   examples/snack-bar/src/app/components/kind/kind.component.ts

--- examples/snack-bar/src/app/components/kind/kind.component.ts
+++ examples/snack-bar/src/app/components/kind/kind.component.ts
@@ -18,7 +18,15 @@ export class KindComponent implements OnInit {
   }
 
   openSnackBar(message: string, action: string) {
-    this._snackBar.open(message, action)
+    this._snackBar.open(
+      message,
+      action,
+      {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'bottom'
+      }
+    )
   }
 
 }
