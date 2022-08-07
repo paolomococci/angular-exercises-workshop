d0101d475a4567c9a93332df7a0b2b16d454a584
paolomococci <paolomococci@gmail.com>
2022-08-07 16:17:59 +0200

    modified:   examples/sort-header/src/app/components/kind/kind.component.ts

--- examples/sort-header/src/app/components/kind/kind.component.ts
+++ examples/sort-header/src/app/components/kind/kind.component.ts
@@ -16,10 +16,10 @@ export class KindComponent implements OnInit {
 
   fruits: Fruit[] = [
     {name: "apricot", water: 91.2, carbohydrates: 6.7, proteins: 0.4, fat: 0.09, fiber: 1.61},
-    {name: "pineapple", water: 97.4, carbohydrates: 0.9, proteins: 0.6,  fat: 0.0, fiber: 1.1},
     {name: "avocado", water: 64, carbohydrates: 2.1, proteins: 6.2,  fat: 24.2, fiber: 3.5},
-    {name: "orange", water: 89.1, carbohydrates: 8.2, proteins: 0.6,  fat: 0.3, fiber: 1.8},
     {name: "banana", water: 78.9, carbohydrates: 17.8, proteins: 1.31,  fat: 0.29, fiber: 1.7},
+    {name: "orange", water: 89.1, carbohydrates: 8.2, proteins: 0.6,  fat: 0.3, fiber: 1.8},
+    {name: "pineapple", water: 97.4, carbohydrates: 0.9, proteins: 0.6,  fat: 0.0, fiber: 1.1},
     {name: "strawberry", water: 91.6, carbohydrates: 5.52, proteins: 0.85,  fat: 0.38, fiber: 1.65}
   ]
 
