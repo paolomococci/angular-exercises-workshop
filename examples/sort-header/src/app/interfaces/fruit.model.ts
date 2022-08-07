4be62f3b23060571902c5780aad85600a4137b33
paolomococci <paolomococci@gmail.com>
2022-08-07 19:15:18 +0200

    modified:   examples/sort-header/src/app/interfaces/fruit.model.ts

--- examples/sort-header/src/app/interfaces/fruit.model.ts
+++ examples/sort-header/src/app/interfaces/fruit.model.ts
@@ -1,8 +1,8 @@
 export interface Fruit {
   name: string
-  water: number | null
-  carbohydrates: number | null
-  proteins: number | null
-  fat: number | null
-  fiber: number | null
+  water: number
+  carbohydrates: number
+  proteins: number
+  fat: number
+  fiber: number
 }
