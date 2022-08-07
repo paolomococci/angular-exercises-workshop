17a60ebb188def078f9b6f62c0413d1255fa800e
paolomococci <paolomococci@gmail.com>
2022-08-07 15:51:26 +0200

    modified:   examples/sort-header/src/app/components/kind/kind.component.ts

--- examples/sort-header/src/app/components/kind/kind.component.ts
+++ examples/sort-header/src/app/components/kind/kind.component.ts
@@ -5,12 +5,28 @@ import {
 
 import { Sort } from '@angular/material/sort'
 
+import { Fruit } from 'src/app/interfaces/fruit.model'
+
 @Component({
   selector: 'app-kind',
   templateUrl: './kind.component.html',
   styleUrls: ['./kind.component.sass']
 })
 export class KindComponent implements OnInit {
+
+  fruits: Fruit[] = [
+    {name: "apricot", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+    {name: "", water: null, calories: null, proteins: null, carbs: null, fat: null, fiber: null},
+  ]
 
   constructor() { }
 
