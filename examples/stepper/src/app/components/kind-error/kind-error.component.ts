c6b9c50efe6cc8600ccaf158d61f33afb70f5a9f
paolomococci <paolomococci@gmail.com>
2022-08-10 00:24:28 +0200

    new file:   examples/stepper/src/app/components/kind-error/kind-error.component.ts

---
+++ examples/stepper/src/app/components/kind-error/kind-error.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-kind-error',
+  templateUrl: './kind-error.component.html',
+  styleUrls: ['./kind-error.component.sass']
+})
+export class KindErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
