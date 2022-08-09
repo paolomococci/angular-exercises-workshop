959e2091320f65879a819f3efccb8aceaaac7ffb
paolomococci <paolomococci@gmail.com>
2022-08-10 00:11:05 +0200

    new file:   examples/stepper/src/app/components/kind-vertical/kind-vertical.component.ts

---
+++ examples/stepper/src/app/components/kind-vertical/kind-vertical.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-kind-vertical',
+  templateUrl: './kind-vertical.component.html',
+  styleUrls: ['./kind-vertical.component.sass']
+})
+export class KindVerticalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
