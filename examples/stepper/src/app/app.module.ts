39742ca5c4e76d883651552a8ccbc69c8a192c23
paolomococci <paolomococci@gmail.com>
2022-08-10 00:10:07 +0200

    modified:   examples/stepper/src/app/app.module.ts

--- examples/stepper/src/app/app.module.ts
+++ examples/stepper/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { MainComponent } from './components/main/main.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
 import { KindComponent } from './components/kind/kind.component'
+import { KindVerticalComponent } from './components/kind-vertical/kind-vertical.component'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -16,7 +17,8 @@ import { HelperModule } from './modules/helper/helper.module'
     AppComponent,
     MainComponent,
     NavbarComponent,
-    KindComponent
+    KindComponent,
+    KindVerticalComponent
   ],
   imports: [
     BrowserModule,
