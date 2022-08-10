ccf60b5a995ea5ce17c18d0da815dd4389fc96ac
paolomococci <paolomococci@gmail.com>
2022-08-10 21:50:44 +0200

    new file:   examples/stepper/src/app/components/kind-linear/kind-linear.component.ts

---
+++ examples/stepper/src/app/components/kind-linear/kind-linear.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-kind-linear',
+  templateUrl: './kind-linear.component.html',
+  styleUrls: ['./kind-linear.component.sass']
+})
+export class KindLinearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
