f0d4f9946c31aad762b5151408394acf15975741
paolomococci <paolomococci@gmail.com>
2022-08-11 23:54:03 +0200

    new file:   examples/table/src/app/components/kind-footer/kind-footer.component.ts

---
+++ examples/table/src/app/components/kind-footer/kind-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-footer',
+  templateUrl: './kind-footer.component.html',
+  styleUrls: ['./kind-footer.component.sass']
+})
+export class KindFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
