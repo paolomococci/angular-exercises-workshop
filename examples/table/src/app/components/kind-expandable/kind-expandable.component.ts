a3eda0a6bc4a1238497a0d435af932e7d87f2284
paolomococci <paolomococci@gmail.com>
2022-08-11 23:38:41 +0200

    new file:   examples/table/src/app/components/kind-expandable/kind-expandable.component.ts

---
+++ examples/table/src/app/components/kind-expandable/kind-expandable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-expandable',
+  templateUrl: './kind-expandable.component.html',
+  styleUrls: ['./kind-expandable.component.sass']
+})
+export class KindExpandableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
