a991b8956d854b96dd0e5b0ae5962be75da4537a
paolomococci <paolomococci@gmail.com>
2022-08-11 23:26:30 +0200

    new file:   examples/table/src/app/components/kind-dynamically/kind-dynamically.component.ts

---
+++ examples/table/src/app/components/kind-dynamically/kind-dynamically.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-dynamically',
+  templateUrl: './kind-dynamically.component.html',
+  styleUrls: ['./kind-dynamically.component.sass']
+})
+export class KindDynamicallyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
