12ebdd1001c8e4cab597e4f1b207ddcd142c7f9f
paolomococci <paolomococci@gmail.com>
2022-08-11 23:21:16 +0200

    new file:   examples/table/src/app/components/kind-crud/kind-crud.component.ts

---
+++ examples/table/src/app/components/kind-crud/kind-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-crud',
+  templateUrl: './kind-crud.component.html',
+  styleUrls: ['./kind-crud.component.sass']
+})
+export class KindCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
