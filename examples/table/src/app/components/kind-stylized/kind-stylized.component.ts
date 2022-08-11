316714585e25801e72914fe3288215da1866dac8
paolomococci <paolomococci@gmail.com>
2022-08-11 23:16:31 +0200

    new file:   examples/table/src/app/components/kind-stylized/kind-stylized.component.ts

---
+++ examples/table/src/app/components/kind-stylized/kind-stylized.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-stylized',
+  templateUrl: './kind-stylized.component.html',
+  styleUrls: ['./kind-stylized.component.sass']
+})
+export class KindStylizedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
