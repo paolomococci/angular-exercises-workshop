c00e23ec6765f7429ca5573328f3ae9df5a41c9d
paolomococci <paolomococci@gmail.com>
2022-08-11 23:31:19 +0200

    new file:   examples/table/src/app/components/kind-observable/kind-observable.component.ts

---
+++ examples/table/src/app/components/kind-observable/kind-observable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-observable',
+  templateUrl: './kind-observable.component.html',
+  styleUrls: ['./kind-observable.component.sass']
+})
+export class KindObservableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
