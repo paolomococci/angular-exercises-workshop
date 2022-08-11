de41ad0207b0c1c37d39aa662242460a4e85d01d
paolomococci <paolomococci@gmail.com>
2022-08-11 23:45:28 +0200

    new file:   examples/table/src/app/components/kind-filtering/kind-filtering.component.ts

---
+++ examples/table/src/app/components/kind-filtering/kind-filtering.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-filtering',
+  templateUrl: './kind-filtering.component.html',
+  styleUrls: ['./kind-filtering.component.sass']
+})
+export class KindFilteringComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
