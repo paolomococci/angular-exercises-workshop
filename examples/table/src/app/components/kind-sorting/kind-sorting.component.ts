d1b65e4d295b7642cc00bfca01babce025c03c3c
paolomococci <paolomococci@gmail.com>
2022-08-12 23:13:33 +0200

    new file:   examples/table/src/app/components/kind-sorting/kind-sorting.component.ts

---
+++ examples/table/src/app/components/kind-sorting/kind-sorting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-sorting',
+  templateUrl: './kind-sorting.component.html',
+  styleUrls: ['./kind-sorting.component.sass']
+})
+export class KindSortingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
