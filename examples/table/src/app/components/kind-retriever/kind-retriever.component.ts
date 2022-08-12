bdcf8483c245aedbc05cfaec81484c919b31c447
paolomococci <paolomococci@gmail.com>
2022-08-12 23:03:57 +0200

    new file:   examples/table/src/app/components/kind-retriever/kind-retriever.component.ts

---
+++ examples/table/src/app/components/kind-retriever/kind-retriever.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-retriever',
+  templateUrl: './kind-retriever.component.html',
+  styleUrls: ['./kind-retriever.component.sass']
+})
+export class KindRetrieverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
