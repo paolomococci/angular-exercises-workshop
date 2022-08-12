932f755275037eafaf846a454d476d7eb30a4922
paolomococci <paolomococci@gmail.com>
2022-08-12 23:09:11 +0200

    new file:   examples/table/src/app/components/kind-multiple/kind-multiple.component.ts

---
+++ examples/table/src/app/components/kind-multiple/kind-multiple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-multiple',
+  templateUrl: './kind-multiple.component.html',
+  styleUrls: ['./kind-multiple.component.sass']
+})
+export class KindMultipleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
