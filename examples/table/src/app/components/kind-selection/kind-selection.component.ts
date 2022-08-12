fbd55039f7d2d6a5e48e0e6e5e2d0e46af51f5b8
paolomococci <paolomococci@gmail.com>
2022-08-12 23:25:48 +0200

    new file:   examples/table/src/app/components/kind-selection/kind-selection.component.ts

---
+++ examples/table/src/app/components/kind-selection/kind-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-selection',
+  templateUrl: './kind-selection.component.html',
+  styleUrls: ['./kind-selection.component.sass']
+})
+export class KindSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
