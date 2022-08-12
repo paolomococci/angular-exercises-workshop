3a697c42224f33b1fbddfdeed33b5d7b6342dd9e
paolomococci <paolomococci@gmail.com>
2022-08-12 23:19:08 +0200

    new file:   examples/table/src/app/components/kind-pagination/kind-pagination.component.ts

---
+++ examples/table/src/app/components/kind-pagination/kind-pagination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-pagination',
+  templateUrl: './kind-pagination.component.html',
+  styleUrls: ['./kind-pagination.component.sass']
+})
+export class KindPaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
