b37e814b6749356b7b33654ac2a29a97b0f29878
paolomococci <paolomococci@gmail.com>
2022-08-13 18:35:25 +0200

    new file:   examples/tabs/src/app/components/kind-bottom/kind-bottom.component.ts

---
+++ examples/tabs/src/app/components/kind-bottom/kind-bottom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-bottom',
+  templateUrl: './kind-bottom.component.html',
+  styleUrls: ['./kind-bottom.component.sass']
+})
+export class KindBottomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
