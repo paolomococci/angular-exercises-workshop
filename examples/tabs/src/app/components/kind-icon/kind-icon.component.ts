7ea122abdc0a0c1ed7e0031149a72b6c5cd23fd2
paolomococci <paolomococci@gmail.com>
2022-08-13 18:52:57 +0200

    new file:   examples/tabs/src/app/components/kind-icon/kind-icon.component.ts

---
+++ examples/tabs/src/app/components/kind-icon/kind-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-icon',
+  templateUrl: './kind-icon.component.html',
+  styleUrls: ['./kind-icon.component.sass']
+})
+export class KindIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
