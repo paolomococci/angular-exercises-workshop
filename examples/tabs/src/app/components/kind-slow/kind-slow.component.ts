a113749529719a02ae6fb6577636c86b899ab28a
paolomococci <paolomococci@gmail.com>
2022-08-13 18:43:42 +0200

    new file:   examples/tabs/src/app/components/kind-slow/kind-slow.component.ts

---
+++ examples/tabs/src/app/components/kind-slow/kind-slow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-slow',
+  templateUrl: './kind-slow.component.html',
+  styleUrls: ['./kind-slow.component.sass']
+})
+export class KindSlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
