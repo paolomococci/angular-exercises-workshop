8dbe0f8889c0bbfd7808e83154894925863547c1
paolomococci <paolomococci@gmail.com>
2022-08-16 00:28:57 +0200

    modified:   examples/table/src/app/enumerations/chemical-group.enumeration.ts

--- examples/table/src/app/enumerations/chemical-group.enumeration.ts
+++ examples/table/src/app/enumerations/chemical-group.enumeration.ts
@@ -1,2 +1,12 @@
 export enum ChemicalGroup {
+  NonMetal = 'nonmetal',
+  NobleGas = 'noble gas',
+  AlkaliMetal = 'alkali metal',
+  AlkalineEarthMetal = 'alkaline earth metal',
+  Metalloid = 'metalloid',
+  Halogen = 'halogen',
+  PostTransitionMetal = 'post-transition metal',
+  TransitionMetal = 'transition metal',
+  Lanthanide = 'lanthanide',
+  Actinide = 'actinide'
 }
