3152f549dce8e5fd97f7c786ab7235c40cae42f3
paolomococci <paolomococci@gmail.com>
2022-08-15 02:31:39 +0200

    modified:   examples/table/src/app/components/kind/kind.component.ts

--- examples/table/src/app/components/kind/kind.component.ts
+++ examples/table/src/app/components/kind/kind.component.ts
@@ -3,6 +3,10 @@ import {
   OnInit
 } from '@angular/core'
 
+import { Element } from 'src/app/models/element.model'
+
+const ELEMENT_DATA: Element[] = []
+
 @Component({
   selector: 'app-kind',
   templateUrl: './kind.component.html',
