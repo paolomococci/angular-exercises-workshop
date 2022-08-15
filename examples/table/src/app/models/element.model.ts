c53f64c9d681fe349b08259237bb05b954c170d9
paolomococci <paolomococci@gmail.com>
2022-08-16 00:29:09 +0200

    modified:   examples/table/src/app/models/element.model.ts

--- examples/table/src/app/models/element.model.ts
+++ examples/table/src/app/models/element.model.ts
@@ -1,8 +1,10 @@
+import { ChemicalGroup } from './../enumerations/chemical-group.enumeration'
+
 export interface Element {
   atomic: number | null
   symbol: string | null
   name: string | null
-  group: string | null
+  group: ChemicalGroup | null
   mass: number | null
   weight: number | null
   radioactive: boolean | null
