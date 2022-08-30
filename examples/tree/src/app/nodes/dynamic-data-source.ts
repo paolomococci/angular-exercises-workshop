6cd2e0c1f8accf4ec4255e5c1bdd1991b1366e4a
paolomococci <paolomococci@gmail.com>
2022-08-30 23:01:45 +0200

    modified:   examples/tree/src/app/nodes/dynamic-data-source.ts

--- examples/tree/src/app/nodes/dynamic-data-source.ts
+++ examples/tree/src/app/nodes/dynamic-data-source.ts
@@ -1,2 +1,18 @@
-export class DynamicDataSource {
+import {
+  CollectionViewer,
+  DataSource
+} from "@angular/cdk/collections"
+import { Observable } from "rxjs"
+
+import { DynamicNode } from "./dynamic-node"
+
+export class DynamicDataSource implements DataSource<DynamicNode> {
+
+  connect(collectionViewer: CollectionViewer): Observable<readonly DynamicNode[]> {
+    throw new Error("Method not implemented.")
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    throw new Error("Method not implemented.")
+  }
 }
