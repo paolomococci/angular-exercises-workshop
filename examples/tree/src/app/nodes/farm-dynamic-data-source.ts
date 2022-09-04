01022da5c3cd75dae3b885e93bf1f7c9f979aa10
paolomococci <paolomococci@gmail.com>
2022-09-04 16:03:35 +0200

    modified:   examples/tree/src/app/nodes/farm-dynamic-data-source.ts

--- examples/tree/src/app/nodes/farm-dynamic-data-source.ts
+++ examples/tree/src/app/nodes/farm-dynamic-data-source.ts
@@ -1,2 +1,17 @@
-export class FarmDynamicDataSource {
+import {
+  CollectionViewer,
+  DataSource
+} from "@angular/cdk/collections"
+import { Observable } from "rxjs"
+
+import { FarmDynamicNode } from "./farm-dynamic-node"
+
+export class FarmDynamicDataSource implements DataSource<FarmDynamicNode> {
+
+  connect(collectionViewer: CollectionViewer): Observable<readonly FarmDynamicNode[]> {
+    throw new Error("Method not implemented.");
+  }
+  disconnect(collectionViewer: CollectionViewer): void {
+    throw new Error("Method not implemented.");
+  }
 }
