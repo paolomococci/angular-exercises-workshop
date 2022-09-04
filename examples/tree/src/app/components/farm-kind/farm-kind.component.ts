d0d124b7a79423b7b7a50a98c497afc7c850b5fb
paolomococci <paolomococci@gmail.com>
2022-09-04 07:59:21 +0200

    new file:   examples/tree/src/app/components/farm-kind/farm-kind.component.ts

---
+++ examples/tree/src/app/components/farm-kind/farm-kind.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-farm-kind',
+  templateUrl: './farm-kind.component.html',
+  styleUrls: ['./farm-kind.component.sass']
+})
+export class FarmKindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
