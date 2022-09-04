e87a07e5f558d2f057441cfb6a09d24595c0f81e
paolomococci <paolomococci@gmail.com>
2022-09-04 17:22:14 +0200

    modified:   examples/tree/src/app/components/farm-kind/farm-kind.component.ts

--- examples/tree/src/app/components/farm-kind/farm-kind.component.ts
+++ examples/tree/src/app/components/farm-kind/farm-kind.component.ts
@@ -2,6 +2,11 @@ import {
   Component,
   OnInit
 } from '@angular/core'
+import {FlatTreeControl} from '@angular/cdk/tree'
+
+import { FarmData } from 'src/app/nodes/farm-data'
+import { FarmDynamicNode } from 'src/app/nodes/farm-dynamic-node'
+import { FarmDynamicDataSource } from 'src/app/nodes/farm-dynamic-data-source'
 
 @Component({
   selector: 'app-farm-kind',
@@ -10,9 +15,33 @@ import {
 })
 export class FarmKindComponent implements OnInit {
 
-  constructor() { }
+  treeControl: FlatTreeControl<FarmDynamicNode>
+
+  dataSource: FarmDynamicDataSource
+
+  constructor(database: FarmData) {
+    this.treeControl = new FlatTreeControl<FarmDynamicNode>(
+      this.getLevel,
+      this.isExpandable
+    )
+    this.dataSource = new FarmDynamicDataSource(
+      this.treeControl,
+      database
+    )
+
+    this.dataSource.data = database.initialData()
+  }
 
   ngOnInit(): void {
   }
+
+  getLevel = (node: FarmDynamicNode) => node.level
+
+  isExpandable = (node: FarmDynamicNode) => node.expandable
+
+  hasChild = (
+    _: number,
+    _nodeData: FarmDynamicNode
+  ) => _nodeData.expandable
 
 }
