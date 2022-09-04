ebac5b01db805becfc17db5ce2185f53874119f2
paolomococci <paolomococci@gmail.com>
2022-09-04 18:20:32 +0200

    new file:   examples/flat-nodes/src/app/app.module.ts

---
+++ examples/flat-nodes/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { MainComponent } from './components/main/main.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { KindComponent } from './components/kind/kind.component'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
+
+import { HelperModule } from './modules/helper/helper.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    KindComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HelperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
