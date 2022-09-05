0c655f0f8fef9d27576edcfa3f1f84373be08a4c
paolomococci <paolomococci@gmail.com>
2022-09-05 06:20:22 +0200

    modified:   examples/flat-nodes/src/app/data/food-data.ts

--- examples/flat-nodes/src/app/data/food-data.ts
+++ examples/flat-nodes/src/app/data/food-data.ts
@@ -3,6 +3,20 @@ import { Food } from "../models/food.model"
 export class FoodData {
 
   static data: Food[] = [
-
+    {
+      name: 'Fruits'
+    },
+    {
+      name: 'Vegetables'
+    },
+    {
+      name: 'Meat'
+    },
+    {
+      name: 'Fish'
+    },
+    {
+      name: 'Drinks'
+    }
   ]
 }
