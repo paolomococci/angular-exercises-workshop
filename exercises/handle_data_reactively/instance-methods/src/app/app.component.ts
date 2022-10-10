64879a11c5cebb204e0208ca01c663958b4c0cbd
paolomococci <paolomococci@gmail.com>
2022-10-10 08:11:41 +0200

    modified:   exercises/handle_data_reactively/instance-methods/src/app/app.component.ts

--- exercises/handle_data_reactively/instance-methods/src/app/app.component.ts
+++ exercises/handle_data_reactively/instance-methods/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'instance-methods';
+  title: string = 'Instance Methods'
+  appId: string = '1F5'
 }
