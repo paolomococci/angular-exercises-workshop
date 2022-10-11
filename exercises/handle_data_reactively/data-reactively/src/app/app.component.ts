6815c4c131386315ef25a68dc83926d07afd814a
paolomococci <paolomococci@gmail.com>
2022-10-11 05:30:58 +0200

    modified:   exercises/handle_data_reactively/data-reactively/src/app/app.component.ts

--- exercises/handle_data_reactively/data-reactively/src/app/app.component.ts
+++ exercises/handle_data_reactively/data-reactively/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'data-reactively';
+  title: string = environment.appTitle
+  appId: string = environment.appId
 }
