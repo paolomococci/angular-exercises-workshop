d54f58b54fea3e537ff0691b39f3c51f0e11119c
paolomococci <paolomococci@gmail.com>
2022-10-12 14:29:36 +0200

    modified:   exercises/handle_data_reactively/static-methods/src/app/repo/data.repo.ts

--- exercises/handle_data_reactively/static-methods/src/app/repo/data.repo.ts
+++ exercises/handle_data_reactively/static-methods/src/app/repo/data.repo.ts
@@ -2,6 +2,21 @@ import { Movie } from "../model/movie.model"
 
 export class Data {
     movies: Movie[] = [
-        {id: '', genre: '', title: ''},
+        {id: 'a7d8c1', genre: '', title: ''},
+        {id: 'a7d8c2', genre: '', title: ''},
+        {id: 'a7d8c3', genre: '', title: ''},
+        {id: 'a7d8c4', genre: '', title: ''},
+        {id: 'a7d8c5', genre: '', title: ''},
+        {id: 'a7d8c6', genre: '', title: ''},
+        {id: 'a7d8c7', genre: '', title: ''},
+        {id: 'a7d8c8', genre: '', title: ''},
+        {id: 'a7d8c9', genre: '', title: ''},
+        {id: 'a7d8ca', genre: '', title: ''},
+        {id: 'a7d8cb', genre: '', title: ''},
+        {id: 'a7d8cc', genre: '', title: ''},
+        {id: 'a7d8cd', genre: '', title: ''},
+        {id: 'a7d8ce', genre: '', title: ''},
+        {id: 'a7d8cf', genre: '', title: ''},
+        {id: 'a7d8d0', genre: '', title: ''},
     ]
 }
