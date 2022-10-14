161c1c2e5230354598f3740b6ea716b42cdc3a69
paolomococci <paolomococci@gmail.com>
2022-10-14 19:43:34 +0200

    modified:   exercises/testing_with_jest/get-start/src/app/app.component.ts

--- exercises/testing_with_jest/get-start/src/app/app.component.ts
+++ exercises/testing_with_jest/get-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'get-start'
+  title = environment.appTitle
+  appId = environment.appId
 }
