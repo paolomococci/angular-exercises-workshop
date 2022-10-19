1fb6818f116ae072e8913f7e1e453a0da3d65c1d
paolomococci <paolomococci@gmail.com>
2022-10-19 15:23:16 +0200

    new file:   exercises/handle_data_reactively/async-pipe/src/app/home/home.component.ts

---
+++ exercises/handle_data_reactively/async-pipe/src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core'
+
+import {
+  Subscription,
+  interval 
+} from 'rxjs'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription | undefined
+
+  inputDataStream: string[] = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ]
+
+  outputDataStream: any = []
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.stopStream()
+  }
+
+  startStream(): void {
+    const streamSource = interval(700)
+    this.subscription = streamSource.subscribe((input) => {
+      this.outputDataStream.push(input)
+      console.log('outputDataStream=>', input)
+    })
+  }
+
+  stopStream(): void {
+    this.subscription?.unsubscribe()
+    this.subscription = undefined
+  }
+}
