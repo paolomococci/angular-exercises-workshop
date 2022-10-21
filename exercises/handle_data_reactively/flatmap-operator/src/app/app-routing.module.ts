95045a5c4f20119616c43973ee8a978c88a10cf8
paolomococci <paolomococci@gmail.com>
2022-10-21 19:20:22 +0200

    modified:   exercises/handle_data_reactively/flatmap-operator/src/app/app-routing.module.ts

--- exercises/handle_data_reactively/flatmap-operator/src/app/app-routing.module.ts
+++ exercises/handle_data_reactively/flatmap-operator/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
+import { UserDetailComponent } from './components/user-detail/user-detail.component'
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'user/uuid',
+    component: UserDetailComponent
   }
 ]
 
