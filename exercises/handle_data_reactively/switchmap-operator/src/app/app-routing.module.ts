ccd7c705029fda8dbd2952a4dd7b845e0592535f
paolomococci <paolomococci@gmail.com>
2022-10-21 19:21:26 +0200

    modified:   exercises/handle_data_reactively/switchmap-operator/src/app/app-routing.module.ts

--- exercises/handle_data_reactively/switchmap-operator/src/app/app-routing.module.ts
+++ exercises/handle_data_reactively/switchmap-operator/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './components/home/home.component'
+import { UserDetailComponent } from './components/user-detail/user-detail.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'user/uuid',
+    component: UserDetailComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
