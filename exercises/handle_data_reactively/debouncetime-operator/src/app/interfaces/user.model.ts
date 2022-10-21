5e4fc90d5f8dfb1e341d787939c87a14b5bb281d
paolomococci <paolomococci@gmail.com>
2022-10-21 21:26:14 +0200

    modified:   exercises/handle_data_reactively/debouncetime-operator/src/app/interfaces/user.model.ts

--- exercises/handle_data_reactively/debouncetime-operator/src/app/interfaces/user.model.ts
+++ exercises/handle_data_reactively/debouncetime-operator/src/app/interfaces/user.model.ts
@@ -1,2 +1,18 @@
 export interface User {
+  login: {
+    uuid: string
+  }
+  name: {
+    title: string
+    first: string
+    last: string
+  }
+  email: string
+  phone: string
+  cell: string
+  picture: {
+    large: string
+    medium: string
+    thumbnail: string
+  }
 }
