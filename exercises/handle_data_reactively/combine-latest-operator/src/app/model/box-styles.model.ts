fe6f382d1e9078b70b35901d2a0d324baf1a0213
paolomococci <paolomococci@gmail.com>
2022-10-21 06:31:38 +0200

    modified:   exercises/handle_data_reactively/combine-latest-operator/src/app/model/box-styles.model.ts

--- exercises/handle_data_reactively/combine-latest-operator/src/app/model/box-styles.model.ts
+++ exercises/handle_data_reactively/combine-latest-operator/src/app/model/box-styles.model.ts
@@ -1,4 +1,11 @@
 import { Observable } from "rxjs"
 
 export interface BoxStyles {
+    styles: Observable<{
+        backgroundColor: string
+        textColor: string
+        borderRadius: string
+        width: string
+        height: string
+      }>
 }
