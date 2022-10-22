a24d8f2e43c96d1c2948a80183d45a284d12eeb3
paolomococci <paolomococci@gmail.com>
2022-10-22 06:53:05 +0200

    modified:   exercises/handle_data_reactively/debouncetime-operator/src/app/services/user.service.ts

--- exercises/handle_data_reactively/debouncetime-operator/src/app/services/user.service.ts
+++ exercises/handle_data_reactively/debouncetime-operator/src/app/services/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { map } from 'rxjs/operators'
-import { Observable } from 'rxjs'
+import {
+  Observable,
+  map
+}  from 'rxjs'
 
 import { User } from '../interfaces/user.model'
 
