7a6ec310db833adc1d3cb90c929aa0b58987d9f9
paolomococci <paolomococci@gmail.com>
2022-10-22 06:51:13 +0200

    modified:   exercises/handle_data_reactively/switchmap-operator/src/app/services/user.service.ts

--- exercises/handle_data_reactively/switchmap-operator/src/app/services/user.service.ts
+++ exercises/handle_data_reactively/switchmap-operator/src/app/services/user.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
+import { User } from '../interfaces/user.model'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
 
-  constructor() { }
+  usersApiUrl: string = ''
+  commentsJsonUrl: string = ''
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getUser(userId: string): Observable<User | undefined> {
+    return this.http.get<{
+      results: User[]
+    }>(
+      this.usersApiUrl
+    ).pipe(
+      map((response) =>
+        response.results.find(
+          (user) => {
+            return user.login.uuid === userId
+          }
+        )
+      )
+    )
+  }
+
+  getSimilarUsers(userId: string): Observable<User[]> {
+    return this.http.get<{
+      results: User[]
+    }>(
+      this.usersApiUrl
+    ).pipe(
+      map(
+        (response) => response.results.filter(
+          user => user.login.uuid !== userId
+        )
+      )
+    )
+  }
+
+  getDataComments(): Observable<string> {
+    return this.http.get<{
+      __comments: string
+    }>(this.commentsJsonUrl).pipe(
+      map((response) => response.__comments)
+    )
+  }
 }
