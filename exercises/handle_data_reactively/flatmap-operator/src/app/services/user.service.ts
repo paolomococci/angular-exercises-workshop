3de1ef06410398e146b2ca2eda5c98edf5d42534
paolomococci <paolomococci@gmail.com>
2022-10-22 06:43:01 +0200

    modified:   exercises/handle_data_reactively/flatmap-operator/src/app/services/user.service.ts

--- exercises/handle_data_reactively/flatmap-operator/src/app/services/user.service.ts
+++ exercises/handle_data_reactively/flatmap-operator/src/app/services/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs'
 import { User } from '../interfaces/user.model'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
 
@@ -15,11 +15,43 @@ export class UserService {
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {}
 
-  getUser(userId: string) {}
+  getUser(userId: string): any {
+    return this.http.get<{
+      results: User[]
+    }>(
+      this.usersApiUrl
+    ).pipe(
+      map((response) =>
+        response.results.find(
+          (user) => {
+            return user.login.uuid === userId
+          }
+        )
+      )
+    )
+  }
 
-  getSimilarUsers(userId: string) {}
+  getSimilarUsers(userId: string): Observable<User[]> {
+    return this.http.get<{
+      results: User[]
+    }>(
+      this.usersApiUrl
+    ).pipe(
+      map(
+        (response) => response.results.filter(
+          user => user.login.uuid !== userId
+        )
+      )
+    )
+  }
 
-  getDataComments() {}
+  getDataComments(): Observable<string> {
+    return this.http.get<{
+      __comments: string
+    }>(this.commentsJsonUrl).pipe(
+      map((response) => response.__comments)
+    )
+  }
 }
