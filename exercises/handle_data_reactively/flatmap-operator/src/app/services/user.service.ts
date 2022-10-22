440daae2bccef899f30f0ed4f1118a50c265cbcd
paolomococci <paolomococci@gmail.com>
2022-10-22 05:29:47 +0200

    modified:   exercises/handle_data_reactively/flatmap-operator/src/app/services/user.service.ts

--- exercises/handle_data_reactively/flatmap-operator/src/app/services/user.service.ts
+++ exercises/handle_data_reactively/flatmap-operator/src/app/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
+import { User } from '../interfaces/user.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  usersApiUrl: string = ''
+  commentsJsonUrl: string = ''
 
   constructor() { }
 }
